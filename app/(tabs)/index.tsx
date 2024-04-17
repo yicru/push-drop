74bf770992e75729b7911861627aa31030de5e60
yuichiro miyamae <yu1ro.mya@gmail.com>
2024-04-17 23:03:05 +0900

    remove: unused code

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,18 +1,9 @@
-import { StyleSheet } from 'react-native'
-
-import EditScreenInfo from '@/components/EditScreenInfo'
-import { Text, View } from '@/components/Themed'
+import { StyleSheet, Text, View } from 'react-native'
 
 export default function TabOneScreen() {
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Tab One</Text>
-      <View
-        style={styles.separator}
-        lightColor="#eee"
-        darkColor="rgba(255,255,255,0.1)"
-      />
-      <EditScreenInfo path="app/(tabs)/index.tsx" />
     </View>
   )
 }
@@ -26,10 +17,5 @@ const styles = StyleSheet.create({
   title: {
     fontSize: 20,
     fontWeight: 'bold',
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
   },
 })
--- app/+html.tsx
+++ app/+html.tsx
@@ -35,8 +35,4 @@ const responsiveBackground = `
 body {
   background-color: #fff;
 }
-@media (prefers-color-scheme: dark) {
-  body {
-    background-color: #000;
-  }
-}`
+`
